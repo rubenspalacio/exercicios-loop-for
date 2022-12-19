6187c6155fd4b1178fdba7904d2518b2217245c0
Rubens Palacio <scaurirubens@gmail.com>
2022-12-19 20:54:57 -0300

    Primeiro commit exercícios de loop for.

---
+++ exercicios-loop-for.js
@@ -0,0 +1,52 @@
+let num = 10;
+
+// Múltiplos de 10.
+
+// for(i = 0; i <= 10; i++){
+//     console.log(num * i)
+// }
+
+
+// Pontuar só os múltiplos de 10.
+
+// for(i = 1; i <= 100; i++){
+//     if(i % 10 == 0) {
+//         console.log(i + " é múltiplo de 10")
+//     }
+//     else{
+//         console.log(i)
+//     } 
+// }
+
+
+// Imprimir "Estou aprendendo loops." 10 vezes
+
+// for(i = 0; i <= 9; i++){
+//     console.log("Estou aprendendo loops.")
+// }
+
+
+// Imprimir a tabuada do 7 com a expressão
+
+// for(i = 1; i <= 10; i++){
+//     console.log("7 X " + i + " = " + (7 * i));
+// }
+
+
+// Dizer quais números são pares de 1 a 20.
+
+// for(i = 1; i <= 20; i++){
+//     if(i % 2 ==0){
+//         console.log(i + " este número é PAR.")
+//     }
+//     else{
+//         console.log(i + " este número é IMPAR")
+//     }
+// }
+
+
+//Imprimir o contador 15 vezes começando pelo 1.
+
+for(i = 1; i <= 15; i++){
+    console.log(i)
+}
